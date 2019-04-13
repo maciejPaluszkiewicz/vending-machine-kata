861d6ce8be2e59f159b5d5750263c1e4b7143a1e
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2019-04-13 22:56:39 +0200

    add hover events

--- jasmine/src/main.js
+++ jasmine/src/main.js
@@ -1,6 +1,8 @@
 
     const vendingMachine = new VendingMachine();
 
+    const messageArray = [];
+
     const displayObserver = {
         update: function(){
             document.querySelector("#message").innerHTML = vendingMachine.currentMessage;
@@ -9,40 +11,26 @@
 
     vendingMachine.subscribe(displayObserver);
 
-    document.querySelector("#nickel").addEventListener("click",nickelListener)
-    document.querySelector("#dime").addEventListener("click",dimeListener);
-    document.querySelector("#quarter").addEventListener("click",quarterListener);
-    document.querySelector("#cap").addEventListener("click",capListener);
-    document.querySelector("#cola").addEventListener("click",colaListener);
-    document.querySelector("#chips").addEventListener("click",chipsListener);
-    document.querySelector("#candy").addEventListener("click",candyListener);
-    document.querySelector("#returnMoney").addEventListener("click",returnMoneyListener);
+    document.querySelector("#nickel").addEventListener("click", function(){animateCoin(5)});
+    document.querySelector("#dime").addEventListener("click",function(){animateCoin(3)});
+    document.querySelector("#quarter").addEventListener("click",function(){animateCoin(6)});
+    document.querySelector("#cap").addEventListener("click",function(){animateCoin(100)});
+    document.querySelector("#cola").addEventListener("click",function(){vendingMachine.selectProduct("cola")});
+    document.querySelector("#chips").addEventListener("click",function(){vendingMachine.selectProduct("chips")});
+    document.querySelector("#candy").addEventListener("click",function(){vendingMachine.selectProduct("candy")});
+    document.querySelector("#returnMoney").addEventListener("click",function(){vendingMachine.giveBackMyMoney()});
 
-    function nickelListener(){
-        animateCoin(5);
-        
-    }
-    function dimeListener(){
-        animateCoin(3);
-    }
-    function quarterListener(){
-        animateCoin(6);
-    }
-    function capListener(){
-        animateCoin(100);
-    }
-    function colaListener(){
-        vendingMachine.selectProduct("cola");
-    }
-    function chipsListener(){
-        vendingMachine.selectProduct("chips");
-    }
-    function candyListener(){
-        vendingMachine.selectProduct("candy");
-    }
-    function returnMoneyListener(){
-        vendingMachine.giveBackMyMoney();
-    }
+    document.querySelector("#nickel").addEventListener("mouseover",function(){showInfoMessage("This is Nickel coin.")})
+    document.querySelector("#dime").addEventListener("mouseover",function(){showInfoMessage("This is Dime coin.")});
+    document.querySelector("#quarter").addEventListener("mouseover",function(){showInfoMessage("This is Quarter coin.")});
+    document.querySelector("#cap").addEventListener("mouseover",function(){showInfoMessage("This is Nuka Cola bottlecap.")});
+    document.querySelector("#cola").addEventListener("mouseover",function(){showInfoMessage("This is Nuka Cola.")});
+    document.querySelector("#chips").addEventListener("mouseover",function(){showInfoMessage("This is bad idea to eat.")});
+    document.querySelector("#candy").addEventListener("mouseover",function(){showInfoMessage("This is bonus to INT stat.")});
+    document.querySelector("#returnMoney").addEventListener("mouseover",function(){showInfoMessage("This is rusty button.")});
+
+    document.querySelector(".display").addEventListener("mouseover",function(){showInfoMessage("This is display of machine.")});
+    // document.querySelector(".vmachine").addEventListener("mouseover",function(){showInfoMessage("Still working. Amazing.")});
 
     function animateCoin(weight){
         let coinName = "#" + vendingMachine.checkCoinByWeight(weight);
@@ -53,4 +41,16 @@
             coinDOMelement.classList.remove("coinMoving");
             vendingMachine.insertCoin(weight);
         }, 1500);
+    }
+
+    function showInfoMessage(infoMessage){
+        if(messageArray.length === 3){
+            messageArray.shift();
+        }
+        messageArray.push(infoMessage);
+        updateMessenger();
+    }
+
+    function updateMessenger(){
+        document.querySelector("#infoMessage").innerHTML = messageArray.join("<br>");
     }
